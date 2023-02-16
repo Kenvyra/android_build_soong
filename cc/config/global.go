676da902827b3f7e3fdb7f7c51c470c371fa0db0
Yi Kong <yikong@google.com>
2023-02-16 17:42:27 +0900

    Update clang version to clang-r487747
    Test: presubmit
    Bug: 264965700
    Change-Id: I2db879f4dcc77b606c788b3e3890ed2a3faa6594

--- cc/config/global.go
+++ cc/config/global.go
@@ -195,6 +195,10 @@ var (
 	}
 
 	noOverrideGlobalCflags = []string{
+		// Workaround for boot loop caused by stack protector.
+		// http://b/267839238
+		"-mllvm -disable-check-noreturn-call",
+
 		"-Werror=bool-operation",
 		"-Werror=implicit-int-float-conversion",
 		"-Werror=int-in-bool-context",
@@ -256,6 +260,7 @@ var (
 		"-Wno-bitwise-instead-of-logical",
 		"-Wno-misleading-indentation",
 		"-Wno-array-parameter",
+		"-Wno-gnu-offsetof-extensions",
 	}
 
 	// Extra cflags for external third-party projects to disable warnings that
@@ -308,8 +313,8 @@ var (
 
 	// prebuilts/clang default settings.
 	ClangDefaultBase         = "prebuilts/clang/host"
-	ClangDefaultVersion      = "clang-r475365b"
-	ClangDefaultShortVersion = "16.0.2"
+	ClangDefaultVersion      = "clang-r487747"
+	ClangDefaultShortVersion = "17"
 
 	// Directories with warnings from Android.bp files.
 	WarningAllowedProjects = []string{
