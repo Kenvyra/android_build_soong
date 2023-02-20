yaap/build_soong

lto: force full in place of thin
We do not care about compile times for runtime improvement.

cc/lto.go
@@ -100,11 +100,9 @@ func (lto *lto) flags(ctx BaseModuleContext, flags Flags) Flags {
 
 	if lto.LTO() {
 		var ltoFlag string
-		if lto.ThinLTO() {
-			ltoFlag = "-flto=thin -fsplit-lto-unit"
-		} else {
-			ltoFlag = "-flto"
-		}
+		// HACK: use full lto opposed to thin lto.
+		// We do not care about compile times for runtime improvement.
+		ltoFlag = "-flto"
 
 		flags.Local.CFlags = append(flags.Local.CFlags, ltoFlag)
 		flags.Local.LdFlags = append(flags.Local.LdFlags, ltoFlag)

Kenvyra/android_build_soong

Change-Id: Ideebf59d763c55445cd53add4f68df0e6ad16b3c

cc/lto.go
@@ -91,14 +91,9 @@ func (lto *lto) flags(ctx BaseModuleContext, flags Flags) Flags {
 	if lto.LTO(ctx) {
 		var ltoCFlag string
 		var ltoLdFlag string
-		if lto.ThinLTO() {
-			ltoCFlag = "-flto=thin -fsplit-lto-unit"
-		} else if lto.FullLTO() {
-			ltoCFlag = "-flto"
-		} else {
-			ltoCFlag = "-flto=thin -fsplit-lto-unit"
-			ltoLdFlag = "-Wl,--lto-O0"
-		}
+		// HACK: use full lto opposed to thin lto.
+		// We do not care about compile times for runtime improvement.
+		ltoCFlag = "-flto"
 
 		flags.Local.CFlags = append(flags.Local.CFlags, ltoCFlag)
 		flags.Local.LdFlags = append(flags.Local.LdFlags, ltoCFlag)